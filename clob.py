7ed3e0678906a826673e00828e1e187651c96ce0
Andre de Paula <andrepps22@gmail.com>
2022-08-11 08:54:27 -0300

    ""

--- clob.py
+++ clob.py
@@ -3,6 +3,7 @@
 ###          DT 10/08/2022            ####
 ##########################################
 
+# Arquivo aonde está armazenado o select do banco.
 from query import select_ora
 import base64
 import cx_Oracle
@@ -14,14 +15,18 @@ password = input(str('Digite a senha do banco: '))
 host = input(str('Digite o nome do HOST: '))
 
 
+# Função que vai conectar ao Oracle.
 def connOra(user, password, host, select_ora):
-    diretorio = ('arquivos/')
+    diretorio = ('arquivos/')  # Diretorio existente
 
     conn = cx_Oracle.connect(user=user, password=password, dsn=host)
     cursor = conn.cursor()
+
+    # Fimitando a quantidade de linhas que vem do banco para a variavel
     cursor.prefetchrows = 1000
     cursor.arraysize = 1000
 
+    # Faz a checagem do tip da conexão e caso seja do typo CLOB retorna os dados em tipo STRING
     def output_type_handler(cursor, name, default_type, size, precision, scale):
         if default_type == cx_Oracle.DB_TYPE_CLOB:
             return cursor.var(cx_Oracle.DB_TYPE_LONG, arraysize=cursor.arraysize)
