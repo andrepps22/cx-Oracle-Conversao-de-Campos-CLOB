c389755017e37027dd80551d1181d14a95c9225d
Andre de Paula <andrepps22@gmail.com>
2022-08-12 08:14:51 -0300

    "informações a mais na documentação."

--- clob.py
+++ clob.py
@@ -3,8 +3,7 @@
 ###          DT 10/08/2022            ####
 ##########################################
 
-# Arquivo aonde está armazenado o select do banco.
-from query import select_ora
+from query import select_ora  # Arquivo aonde está armazenado o select do banco.
 import base64
 import cx_Oracle
 import datetime
@@ -22,11 +21,11 @@ def connOra(user, password, host, select_ora):
     conn = cx_Oracle.connect(user=user, password=password, dsn=host)
     cursor = conn.cursor()
 
-    # Fimitando a quantidade de linhas que vem do banco para a variavel
+    # Filtrando a quantidade de linhas que vem do banco para a variavel
     cursor.prefetchrows = 1000
     cursor.arraysize = 1000
 
-    # Faz a checagem do tip da conexão e caso seja do typo CLOB retorna os dados em tipo STRING
+    # Faz a checagem do tipo da conexão e caso seja do typo CLOB retorna os dados em tipo STRING
     def output_type_handler(cursor, name, default_type, size, precision, scale):
         if default_type == cx_Oracle.DB_TYPE_CLOB:
             return cursor.var(cx_Oracle.DB_TYPE_LONG, arraysize=cursor.arraysize)
@@ -36,18 +35,20 @@ def connOra(user, password, host, select_ora):
     start = datetime.datetime.now()
     cursor.execute(select_ora)
 
+    # Transforma as linhas que vieram do banco em arquivos.
     while True:
 
         row = cursor.fetchone()
         if not row:
             break
-        res = bytes(row[2], 'utf-8')
+        res = bytes(row[2], 'utf-8')  #'ROW[2]' são os dados brutos quem vem em forma de testo, a variavel 'res' recebe o arquivo tranformado em bytes
 
-        bt = base64.decodebytes(res)
+        bt = base64.decodebytes(res)  # A variavel bt recebi os bytes decodificados.
 
-        with open(diretorio + row[1], "wb+") as f:
+        with open(diretorio + row[1], "wb+") as f:  # Gera os arquivs que vem  'ROW[1]' nome e tipo do arquivo
             f.write(bt)
 
+    cursor.close()
     conn.close()
 
 
